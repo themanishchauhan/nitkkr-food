a4a5f0c9bf7fe08e63379155d3bf266833ac58b8
Manish Chauhan <manishchauhan@Manishs-MacBook-Air.local>
2026-08-26 01:02:55 +0530

    fix: export GET in api/search.ts

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,2 +1,8 @@
-export { GET } from './search.json';
+import type { APIRoute } from 'astro';
+import { GET as searchJsonGet } from './search.json';
+
 export const prerender = false;
+
+export const GET: APIRoute = async (context) => {
+  return searchJsonGet(context);
+};
